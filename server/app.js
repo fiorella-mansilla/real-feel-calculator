adaf6f489423a9302a73959a97a0e2c44d6b5fec
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2025-01-16 17:26:19 +0100

    Installed and set CORS dependency

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import weatherRoutes from './routes/weatherRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+
 // Use JSON parser middleware
 app.use(express.json());
 
 // Use weather routes
-app.use('/api/weather', weatherRoutes);
+app.use('/api', weatherRoutes);
 
 // Start the server
 app.listen(PORT, () => {
