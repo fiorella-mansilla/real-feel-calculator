32545d85b5b9158883a9ca359b83ce6cb11c63e9
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2025-01-26 11:10:42 +0100

    Improved code for separation of concerns and updated comments

--- client/src/services/weatherService.js
+++ client/src/services/weatherService.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 /**
- * Fetches the real-feel temperature and weather data for a given city.
+ * Fetches the real-feel temperature for a selected city using the backend API, 
+ * which in turn retrieves real-time weather data from the external API "Bright Sky".
  *
  * @param {string} lat - Latitude of the city.
  * @param {string} lon - Longitude of the city.
  * @returns {Promise<Object>} The fetched weather data.
  */
+
 export const fetchRealFeel = async (lat, lon) => {
   if (!lat || !lon) throw new Error("Latitude and longitude are required.");
 
